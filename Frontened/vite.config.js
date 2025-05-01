bbbeed8a4893db3b228133e53f77004a26029094
Sumit Tank <sumittank77756@gmail.com>
2025-05-01 23:23:14 +0530

    Added Netlify _redirects support and other updates

--- Frontened/vite.config.js
+++ Frontened/vite.config.js
@@ -1,7 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',       // your _redirects file
+          dest: '.'                // copy to dist/
+        }
+      ]
+    })
+  ]
 })
